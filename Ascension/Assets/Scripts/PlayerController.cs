6c5488d3da83e2d642f951756a3978f8c22dc800
dillonsherling <dillon.sherling@wsu.edu>
2024-11-09 17:43:08 -0800

    added jump functionality and AnimationStrings class

--- Ascension/Assets/Scripts/PlayerController.cs
+++ Ascension/Assets/Scripts/PlayerController.cs
@@ -1,15 +1,16 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-[RequireComponent (typeof(Rigidbody2D))]
+[RequireComponent (typeof(Rigidbody2D), typeof(TouchingDirections))]
 public class PlayerController : MonoBehaviour
 {
     Vector2 moveInput;
     Rigidbody2D rb;
+    TouchingDirections touchingDirections;
 
     public float WalkSpeed = 5f;
     public bool IsMoving { get; set; }
-
+    public float JumpImpulse = 10f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -29,6 +30,7 @@ public class PlayerController : MonoBehaviour
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        touchingDirections = GetComponent<TouchingDirections>();
     }
 
     /// <summary>
@@ -37,9 +39,25 @@ public class PlayerController : MonoBehaviour
     /// <param name="context">Movement callback.</param>
     public void OnMove(InputAction.CallbackContext context)
     {
+        if (context.started)
+        {
+            IsMoving = true;
+        }
+        else if (context.canceled)
+        {
+            IsMoving = false;
+        }
         moveInput = context.ReadValue<Vector2>();
-
         IsMoving = moveInput != Vector2.zero;
+
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.started && touchingDirections.IsGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, JumpImpulse);
+        }
     }
 
     private void FixedUpdate()
