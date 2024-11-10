e7fcc53d44765b9a682672ebd5b530c1e9d82943
Nick2k4L <nico2k4@gmail.com>
2024-11-10 13:47:33 -0800

    Created Player-Animations
    This is for player animations such as jumping and moving.

--- Ascension/Assets/Scripts/PlayerController.cs
+++ Ascension/Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -6,6 +7,7 @@ public class PlayerController : MonoBehaviour
 {
     Vector2 moveInput;
     Rigidbody2D rb;
+<<<<<<< Updated upstream
     Animator animator; // animation shiii
 
     public float WalkSpeed = 5f;
@@ -15,37 +17,81 @@ public class PlayerController : MonoBehaviour
 
     public bool IsMoving // Will help with the animation stuff
     {
+=======
+    TouchingDirections touchingDirections;
+    Animator animator; // For animations and shit.
+
+    public float WalkSpeed = 5f;
+    public bool IsMoving 
+    { 
+>>>>>>> Stashed changes
         get
         {
             return _isMoving;
         }
+<<<<<<< Updated upstream
         private set
+=======
+        set
+>>>>>>> Stashed changes
         {
             _isMoving = value;
             animator.SetBool("isMoving", value);
         }
     }
 
+<<<<<<< Updated upstream
     [SerializeField]
     private bool _isJumping = false; // default is set to false
 
+=======
+>>>>>>> Stashed changes
     public bool IsJumping
     {
         get
         {
             return _isJumping;
         }
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
         set
         {
             _isJumping = value;
             animator.SetBool("isJumping", value);
         }
+<<<<<<< Updated upstream
 
     }
 
 
 
+=======
+    }
+
+    public bool IsFacingRight { get { return _isFacingRight; } private set {
+            if (_isFacingRight != value)
+            {
+                // Flip the local scale
+                transform.localScale *= new Vector2(-1, 1);
+            }
+            _isFacingRight = value;
+        
+        } }
+
+    public float JumpImpulse = 10f;
+>>>>>>> Stashed changes
+
+    [SerializeField]
+    private bool _isMoving = false; // Keeping track if the player is moving or not.
+
+    [SerializeField]
+    private bool _isJumping = false; // Keeping track if the player is jumping or not.
+
+    public bool _isFacingRight = true;
+
+  
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -65,6 +111,10 @@ public class PlayerController : MonoBehaviour
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+<<<<<<< Updated upstream
+=======
+        touchingDirections = GetComponent<TouchingDirections>();
+>>>>>>> Stashed changes
         animator = GetComponent<Animator>();
     }
 
@@ -77,6 +127,39 @@ public class PlayerController : MonoBehaviour
         moveInput = context.ReadValue<Vector2>();
 
         IsMoving = moveInput != Vector2.zero;
+<<<<<<< Updated upstream
+=======
+
+        SetFacingDirection(moveInput);
+
+    }
+
+    private void SetFacingDirection(Vector2 moveInput)
+    {
+        if (moveInput.x > 0 && !IsFacingRight)
+        {
+            // face the right
+            IsFacingRight = true;
+        }
+        else if (moveInput.x < 0 && IsFacingRight) 
+        {
+            // face the left
+            IsFacingRight = false;
+        }
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.started && touchingDirections.IsGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, JumpImpulse);
+            IsJumping = true;
+        }
+        else if(context.canceled)
+        {
+            IsJumping = false;
+        }
+>>>>>>> Stashed changes
     }
 
     private void FixedUpdate()
